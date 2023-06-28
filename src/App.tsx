9f6fc8f2c3b9711bb4b5a25a988adb5cd1e5243a
Aliaksandr91 <horbachalex91@gmail.com>
2023-06-28 09:51:02 +0200

    feat: lessons 1 - 10

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Technologies from "./Technologies";
+import Header from "./Header";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Technologies/>
+    </div>
+
+  );
+}
+
+
+
+export default App;
