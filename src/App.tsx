aebb34bede97533fcc9b1a80ebc841d982a4b691
Aliaksandr91 <horbachalex91@gmail.com>
2023-06-28 11:26:34 +0200

    feat: 11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,30 @@ import React from 'react';
 import Technologies from "./Technologies";
 import Header from "./Header";
 import './App.css';
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div>
-        <Header/>
-        <Technologies/>
+    <div className="app-wrapper">
+        <header className="header">
+            <img src="" alt=""/>
+        </header>
+        <nav className='nav'>
+            <div><a href="#">Profile</a></div>
+            <div><a href="#">Messages</a></div>
+            <div><a href="#">News</a></div>
+            <div><a href="#">Music</a></div>
+            <div><a href="#">Settings</a></div>
+        </nav>
+        <div className='content'>
+            <img src="https://life.fakty.com.ua/wp-content/uploads/sites/3/2022/03/27/novyj-proekt-10-620x349.png" alt=""/>
+            <div><img src="https://icdn.lenta.ru/images/2021/04/27/16/20210427163138131/square_320_c09ebae17387b7d6eeb9fa0d42afe5ee.jpg" alt=""/></div>
+        <div>My posts
+            <div>new post</div>
+            <div>Post 1</div>
+            <div>post 2</div>
+        </div>
+        </div>
     </div>
 
   );
