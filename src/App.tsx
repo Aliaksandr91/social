72dd0bb0210bd3f9cdf9384600d360c12e7215ff
Aliaksandr91 <horbachalex91@gmail.com>
2023-07-20 17:24:58 +0200

    feat: lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 
 function App() {
@@ -12,8 +16,15 @@ function App() {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile/>*/}
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/profile" element={<Profile />}/>
+                        <Route path="/dialogs" element={<Dialogs />}/>
+                        <Route path="/news" element={<News />}/>
+                        <Route path="/music" element={<Music />}/>
+                        <Route path="/settings" element={<Settings />}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
 
         </div>
