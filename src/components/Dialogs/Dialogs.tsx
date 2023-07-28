c36a0fb3b3386ee15ee4ffeef73965aed274650d
Aliaksandr91 <horbachalex91@gmail.com>
2023-07-28 16:08:47 +0200

    feat: lesson 24

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -9,6 +9,15 @@ type PropsType = {
 type PropsTypeMessage = {
     message: string
 }
+
+type DialogsDataType = {
+    id: number
+    name: string
+}
+type MessageDataType = {
+    id: number
+    message: string
+}
 const DialogItem = (props: PropsType) => {
     let path = '/dialogs/' + props.id
     return (
@@ -18,12 +27,31 @@ const DialogItem = (props: PropsType) => {
     )
 }
 
-const Message = (props:PropsTypeMessage) => {
-  return (
-      <div className={classes.message}>{props.message}</div>
-  )
+const Message = (props: PropsTypeMessage) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
 }
 export const Dialogs = () => {
+
+    let dialogsData:DialogsDataType[] = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'}
+    ]
+    let messageData:MessageDataType[] = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is you'},
+        {id: 3, message: 'yo'},
+        {id: 4, message: 'aaa'},
+        {id: 5, message: 'bbbb'},
+        {id: 6, message: 'kkkk'}
+    ]
+
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
+type PostDataTypes = {
+    id: number
+    message: string
+    likesCount:number
+}
 export const MyPosts = () => {
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'Hello, my first post?', likesCount: 11}
+    ]
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
