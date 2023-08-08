8f92d9ef834ffc79c6548ffb505d7de0140657b9
Aliaksandr91 <horbachalex91@gmail.com>
2023-08-08 16:46:45 +0200

    feat: lesson 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import classes from './MyPosts.module.css';
 import { Post } from "./Post/Post";
 import {MyPostsProps, PostData} from "../../../redux/state";
@@ -7,14 +7,21 @@ export const MyPosts = (props:MyPostsProps) => {
     let postsElements = props.postData.map((el: PostData) => (
         <Post message={el.message} likesCount={el.likesCount} />
     ));
+    let newPostElement = useRef<HTMLTextAreaElement>(null)
+    const addPost = () => {
+        if (newPostElement.current !== null) {
+            alert(newPostElement.current.value)
+        }
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea name="" id=""></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={classes.posts}>
                 {postsElements}
