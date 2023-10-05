68dec92b38cf75808b07485d77a3c952f390ae5f
Aliaksandr91 <horbachalex91@gmail.com>
2023-10-05 12:41:05 +0200

    feat: lesson 54

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,15 +4,14 @@ import styles from "./users.module.css";
 import axios from "axios";
 import userPhoto from '../../assets/images/profile-photo.png'
 export class Users extends React.Component<any, any> {
-
-    constructor(props:any) {
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    debugger
-                    this.props.setUsers(response.data.items)
-                })
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                debugger
+                this.props.setUsers(response.data.items)
+            })
     }
+
     render() {
         return <div>
             {
