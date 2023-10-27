299462a4164bd4a4c9bf1cd204fc81383f97de22
Aliaksandr91 <horbachalex91@gmail.com>
2023-10-27 19:44:05 +0200

    feat: lesson 75

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,26 @@
-export const Login = () => {
-    return <h1>Login</h1>
+import {Field, reduxForm} from "redux-form";
+type formDataType ={
+    login:string
+    password:string
+    rememberMe:boolean
 }
+export const Login = () => {
+    const onSubmit = (formData:any) => {
+        console.log(formData)
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+}
+const LoginForm = (props:any) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div><Field placeholder={'Login'} name={'login'} component={'input'}/></div>
+        <div><Field placeholder={'Password'} name={'password'} component={'input'}/></div>
+        <div><Field component={'input'} name={'rememberMe'} type="checkbox"/>remember Me</div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
