fbe6a02faa6897725b657659ea6ad4d96a510046
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-16 18:12:00 +0100

    feat: add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from './Header.module.css'
 import {NavLink} from "react-router-dom";
-
+import Logo from '../../assets/images/logo.png'
 export const Header = (props: any) => {
     return (
         <header className={classes.header}>
-            <img src="https://i.pinimg.com/originals/e9/e2/78/e9e2787d0cb55d570fe1c76843506759.jpg" alt=""/>
+            <img src={Logo} alt=""/>
             <div className={classes.loginBlock}>
                 {
                     props.isAuth
