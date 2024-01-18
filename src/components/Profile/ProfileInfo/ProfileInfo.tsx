42b776b2e620cc3d6d8ea1ea46f9ee7e73aced36
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-18 19:17:44 +0100

    feat: add sprofile styles

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,16 +4,14 @@ import {Loader} from "../../common/Loader/Loader";
 import photo from '../../../assets/images/profile.png'
 import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 
-export const ProfileInfo:React.FC<any> = ({profile, status, updateStatus}) => {
+export const ProfileInfo: React.FC<any> = ({profile, status, updateStatus}) => {
     if (!profile) {
         return <Loader/>
     }
     return (
-        <div>
-            <div className={classes.descriptionBlock}>
-                <img className={classes.photo} src={profile.photos.large ?  profile.photos.large : photo} alt=""/>
-                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
-            </div>
+        <div className={classes.descriptionBlock}>
+            <img className={classes.photo} src={profile.photos.large ? profile.photos.large : photo} alt=""/>
+            <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
     )
 }
