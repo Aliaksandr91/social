30b3ebfe76c6b22693d82b6300974110b30e505d
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-02-01 19:05:16 +0100

    feat: add news api

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./News.module.css";
+
 export const News = () => {
+    useEffect(() => {
+        fetch('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json')
+            .then((response) => response.json())
+            .then(data => console.log(data))
+    }, []);
+
     return (
         <div>
             News
         </div>
-    )
-}
+    );
+};
